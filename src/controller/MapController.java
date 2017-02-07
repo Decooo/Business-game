4b4b1bc3f587e4a194fe34498e71d35a8b49bec4
Decooo <kusnierzjakub@gmail.com>
2017-02-07 15:16:21 +0100

    The addition of the choice number of players and money

--- src/controller/MapController.java
+++ src/controller/MapController.java
@@ -1,4 +1,16 @@
 package controller;
 
-public class MapController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MapController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,18 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/" + "Map.fxml"));
-        primaryStage.setTitle("Business game");
-        primaryStage.setScene(new Scene(root, 1280 , 720));
-        primaryStage.show();
-    }
-
+    private static Stage primaryStage;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void loadFXML(String nameFXML, int width, int height) throws IOException {
+        Parent root = FXMLLoader.load(Main.class.getResource("../view/" + nameFXML + ".fxml"));
+        primaryStage.setTitle("Business game");
+        primaryStage.setScene(new Scene(root, width, height));
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Main.primaryStage = primaryStage;
+        loadFXML("MainMenu", 490, 580);
     }
 }
